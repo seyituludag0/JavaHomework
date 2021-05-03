c8d86fb25a6ab2fc607687876f93b2e4c16dcd3c
seyituludag0 <seyituludag0@gmail.com>
2021-05-03 19:12:05 +0300

    4.Günün 2.Ödevi

---
+++ overriding/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BaseKrediManager[] baseKrediManagers = new BaseKrediManager[] {
+                new TarimKrediManager(), new OgretmenKrediManager(), new OgrenciKrediManager()
+        };
+
+        for (BaseKrediManager baseKrediManager: baseKrediManagers) {
+            System.out.println(baseKrediManager.Hesapla(2000));
+        }
+    }
+}
